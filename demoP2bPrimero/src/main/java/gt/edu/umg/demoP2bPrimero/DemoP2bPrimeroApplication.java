3098a1630dcfc5168cc4a03cfe6d2d0827a4a0db
Nery-Contreras-Tobar <79219978+Nery-Contreras-Tobar@users.noreply.github.com>
2021-10-23 17:14:31 -0600

    tarea 13
    SpringBoot

---
+++ demoP2bPrimero/src/main/java/gt/edu/umg/demoP2bPrimero/DemoP2bPrimeroApplication.java
@@ -0,0 +1,13 @@
+package gt.edu.umg.demoP2bPrimero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoP2bPrimeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoP2bPrimeroApplication.class, args);
+	}
+
+}
